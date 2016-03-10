4d82e42bc0ae6c68276dd79be639009c47578bd7
Kevin Judd <kjudd@engs-24152.local>
2016-03-10 12:30:28 +0000

    ComposePoses.m and tester file.

---
+++ pb-parsers/mrg/Slam/compose_poses.m
@@ -0,0 +1,17 @@
+function [ u ] = compose_poses( poses )
+%     transform = tcomp([poses{end - 1}.x, poses{end - 1}.y, poses{end - 1}.theta],...
+%         [poses{end}.x, poses{end}.y, poses{end}.theta]);
+    if(size(poses,1) == 1)
+        transform = [poses{1}.x; poses{1}.y; poses{1}.theta]; 
+    else
+        transform = tcomp([poses{1}.x, poses{1}.y, poses{1}.theta]',...
+            [poses{2}.x, poses{2}.y, poses{2}.theta]');
+    %     for i = len(poses)-1:-1:1
+        for i = 2:length(poses)
+    %         transform = tcomp([poses{i}.x, poses{i}.y, poses{i}.theta], transform);
+            transform = tcomp(transform, [poses{i}.x, poses{i}.y, poses{i}.theta]');
+        end
+    end
+    u = transform;
+%     u = tinv(transform);
+end
