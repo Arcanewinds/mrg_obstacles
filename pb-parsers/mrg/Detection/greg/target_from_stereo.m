26dc0f44998267797916d6ae54763e54db7856cc
Kevin Judd <kjudd@engs-24152.local>
2016-03-11 11:59:42 +0000

    Target detection integration.

--- pb-parsers/mrg/Detection/greg/target_from_stereo.m
+++ pb-parsers/mrg/Detection/greg/target_from_stereo.m
@@ -8,7 +8,7 @@ function [range, angle] = target_from_stereo(s,camera_model)
     if ~isempty(r_x) && ~isempty(l_x)
         [r_r, r_a] = project_geom(r_x, r_y, camera_model);
         [l_r, l_a] = project_geom(l_x, l_y, camera_model);
-        range = r_r + l_r;
-        angle = r_a + l_a;
+        range = (r_r + l_r)/2;
+        angle = (r_a + l_a)/2;
     end
 end
